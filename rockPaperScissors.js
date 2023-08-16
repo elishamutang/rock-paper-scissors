d9563106e60a92cef4e1f0077b7db5ce90f537ce
elishamutang <elishamutang@outlook.com>
2023-08-16 23:14:54 +0800

    Done creating gameRound function.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,7 +1,47 @@
+// getComputerChoice is working
 function getComputerChoice() {
     let choices = ["Rock", "Paper", "Scissors"];
     let randomChoice = choices[Math.floor(Math.random() * choices.length)];
     return randomChoice;
-}
+} 
 
 console.log(getComputerChoice());
+
+function gameRound(playerSelection, computerSelection) {
+    playerSelection = prompt("Rock, paper or Scissors?");
+    computerSelection = getComputerChoice();
+
+    // Convert inputs to lowercase to compare
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    // Display both selection
+    console.log(`You: ${playerSelection} \nComputer: ${computerSelection}`);
+
+    if (playerSelection == computerSelection) {
+        return "It's a Tie!";
+    } else if (playerSelection == "rock") {
+        if (computerSelection == "paper") {
+            return "Computer wins";
+        } else if (computerSelection == "scissors") {
+            return "You win!";
+        }
+    } else if (playerSelection == "paper") {
+        if (computerSelection == "rock") {
+            return "You win!";
+        } else if (computerSelection == "scissors") {
+            return "Computer wins";
+        }
+    } else if (playerSelection == "scissors") {
+        if (computerSelection == "rock") {
+            return "Computer wins";
+        } else if (computerSelection == "paper") {
+            return "You win!";
+        }
+    } else {
+        return "Invalid entry";
+    }
+
+}
+
+console.log(gameRound());
